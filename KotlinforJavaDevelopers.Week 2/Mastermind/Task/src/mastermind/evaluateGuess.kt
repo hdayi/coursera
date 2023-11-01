434d56d1699cdef6ff05c75724a4db3b296b8a05
HACI DAYI <hdayi@hotmail.com>
2023-11-02 02:40:28 +0300

    test tamam

--- KotlinforJavaDevelopers.Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
+++ KotlinforJavaDevelopers.Week 2/Mastermind/Task/src/mastermind/evaluateGuess.kt
@@ -3,9 +3,16 @@ package mastermind
 data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
-    TODO()
-}
-
-enum class Color{
-    RED, GREEN, BLUE, ORANGE
+    var right = 0;
+    var wrong = 0;
+    for (t in guess){
+        if (t in secret){
+            if (guess.indexOf(t) == secret.indexOf(t)){
+                right ++
+            }else{
+                wrong ++
+            }
+        }
+    }
+    return Evaluation(right, wrong)
 }
